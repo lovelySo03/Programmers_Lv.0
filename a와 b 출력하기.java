dd6411ce7ce996e84388da64c2504b6b83c23978
lovelySo03 <143456833+lovelySo03@users.noreply.github.com>
2023-10-31 23:48:08 +0900

    Create a와 b 출력하기.java
    정수 a와 b가 주어집니다. 각 수를 입력받아 입출력 예와 같은 형식으로 출력하는 코드를 작성해 보세요.

---
+++ a와 b 출력하기.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        // System.out.println(a + b);
+        
+        if(-100000 <= a && b <= 100000){
+            System.out.println("a = "+a);
+            System.out.println("b = "+b);
+        }
+    }
+}
